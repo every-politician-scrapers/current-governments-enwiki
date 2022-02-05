12138b52e6b11a55ad525afe9e0922d23a9b4cd9
Tony Bowden <tony@tmtm.com>
2022-02-05 10:42:38 +0200

    initial scraper

---
+++ compare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bundle exec ruby bin/wikipedia.rb | ifne tee data/wikipedia.csv
+qsv select id data/wikipedia.csv | qsv behead | xargs wd sparql bin/wikidata.js -f csv |
+  sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | ifne tee data/wikidata.csv
